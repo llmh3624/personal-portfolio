a22d9d59c16102832ed29d00e3340b1341689e34
Laura Moragues <llmh3624@gmail.com>
2024-07-29 18:20:47 +0200

    Added projects

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 function filterProjects(language) {
-  var projects = document.getElementsByClassName("project");
+  var projects = document.getElementsByClassName("work-project");
 
   for (var i = 0; i < projects.length; i++) {
     if (
       language === "all" ||
       projects[i].getAttribute("data-language") === language
     ) {
-      projects[i].style.display = "block";
+      projects[i].style.display = "grid";
     } else {
       projects[i].style.display = "none";
     }
   }
 }
 
+// Show all projects by default
 filterProjects("all");
